a5f9ae9a76b433a895f5a52491f7d7a37b7b2dcc
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-09-16 01:56:25 -0400

    baisc setup stetch for test

--- index.test.js
+++ index.test.js
@@ -9,15 +9,36 @@ describe('[Exercise 1] trimProperties', () => {
     expect(actual).toEqual(expected)
   })
   // test('[2] returns a copy, leaving the original object intact', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
 })
 
 describe('[Exercise 2] trimPropertiesMutation', () => {
   // test('[3] returns an object with the properties trimmed', () => {})
   // test('[4] the object returned is the exact same one we passed in', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
 })
 
 describe('[Exercise 3] findLargestInteger', () => {
   // test('[5] returns the largest number in an array of objects { integer: 2 }', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
 })
 
 describe('[Exercise 4] Counter', () => {
@@ -26,8 +47,29 @@ describe('[Exercise 4] Counter', () => {
     counter = new utils.Counter(3) // each test must start with a fresh couter
   })
   // test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
   // test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
   // test('[8] the count eventually reaches zero but does not go below zero', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
 })
 
 describe('[Exercise 5] Seasons', () => {
@@ -36,11 +78,53 @@ describe('[Exercise 5] Seasons', () => {
     seasons = new utils.Seasons() // each test must start with fresh seasons
   })
   // test('[9] the FIRST call of seasons.next returns "summer"', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
   // test('[10] the SECOND call of seasons.next returns "fall"', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
   // test('[11] the THIRD call of seasons.next returns "winter"', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
   // test('[12] the FOURTH call of seasons.next returns "spring"', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
   // test('[13] the FIFTH call of seasons.next returns again "summer"', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
   // test('[14] the 40th call of seasons.next returns "spring"', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
 })
 
 describe('[Exercise 6] Car', () => {
@@ -49,12 +133,54 @@ describe('[Exercise 6] Car', () => {
     focus = new utils.Car('focus', 20, 30) // each test must start with a fresh car
   })
   // test('[15] driving the car returns the updated odometer', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
   // test('[16] driving the car uses gas', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
   // test('[17] refueling allows to keep driving', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
   // test('[18] adding fuel to a full tank has no effect', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
 })
 
 describe('[Exercise 7] isEvenNumberAsync', () => {
   // test('[19] resolves true if passed an even number', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
   // test('[20] resolves false if passed an odd number', () => {})
+  test('[1] returns an object with the properties trimmed', () => {
+    // EXAMPLE
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
 })
